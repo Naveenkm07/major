ac79eaf15125cf9a002fddb3b31b92eee453813e
naveenkm07 <kmnaveenkm85@gmail.com>
2026-06-02 23:13:43 +0530

    Add Vercel deployment configuration

---
+++ mobile_app_flutter/build_vercel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Installing Flutter for Vercel..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Checking Flutter version..."
+flutter --version
+
+echo "Enabling Flutter Web..."
+flutter config --enable-web
+
+echo "Getting dependencies..."
+flutter pub get
+
+echo "Building Flutter Web..."
+flutter build web --release
